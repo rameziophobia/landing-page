76036b250176235c9bb37fb81349bd7720198e9d
Ramez <rameznoshy@gmail.com>
2020-08-23 01:35:09 +0200

    add: starter code

---
+++ js/app.js
@@ -0,0 +1,59 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+ */
+
+/**
+ * Define Global Variables
+ * 
+ */
+
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ * 
+ */
+
+
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+ */
+
+// build the nav
+const sectionElements = document.getElementsByTagName("section");
+
+for (element of sectionElements) {
+    console.log(element.dataset.nav);
+};
+
+// Add class 'active' to section when near top of viewport
+
+
+// Scroll to anchor ID using scrollTO event
+
+
+/**
+ * End Main Functions
+ * Begin Events
+ * 
+ */
+
+// Build menu 
+
+// Scroll to section on link click
+
+// Set sections as active
